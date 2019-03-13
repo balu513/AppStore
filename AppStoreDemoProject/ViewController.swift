28caca74b38750a74a7d073cf2582ff6d8aaf485
balakrishna <balub513@gmail.com>
2019-03-13 10:40:16 +0530

    Initial Commit

---
+++ AppStoreDemoProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreDemoProject
+//
+//  Created by Balakrishna on 13/03/19.
+//  Copyright © 2019 Balakrishna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
